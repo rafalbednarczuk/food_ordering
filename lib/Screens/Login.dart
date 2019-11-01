d35c080123e307d4de8bddce7526befd9457e211
Maadhav <maadhav2001@gmail.com>
2019-11-01 19:32:58 +0530

    Made changes as requested

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,158 +1,159 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:food_ordering/utils/utils.dart';
 
-class SignIn extends StatelessWidget {
+class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          // Background Image
-            image: AssetImage('Assets/pizza.jpg'), 
-            fit: BoxFit.cover),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-              body: Container(
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.transparent,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(23.0),
-            child: ListView(
-              shrinkWrap: true,
-              children: <Widget>[
-                SizedBox(height: 200,),
-                Form(
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                        child: TextFormField(
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/pizza.jpg'), fit: BoxFit.cover),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.transparent,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(23.0),
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  SizedBox(
+                    height: 200,
+                  ),
+                  Form(
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          child: TextFormField(
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                            decoration: InputDecoration(
+                              enabledBorder: const UnderlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              labelText: Strings.username,
+                              labelStyle:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        TextFormField(
                           style: TextStyle(
-        color: Colors.white,
-    ),
+                            color: Colors.white,
+                          ),
+                          obscureText: true,
                           decoration: InputDecoration(
                             enabledBorder: const UnderlineInputBorder(
                               borderSide: const BorderSide(
                                 color: Colors.white,
                               ),
                             ),
-                            labelText: 'Username',
+                            labelText: Strings.password,
                             labelStyle:
                                 TextStyle(fontSize: 15, color: Colors.white),
                           ),
                         ),
-                      ),
-                      TextFormField(
-                        style: TextStyle(
-        color: Colors.white,
-    ),
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          enabledBorder: const UnderlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.white,
-                            ),
-                          ),
-                          labelText: 'Password',
-                          labelStyle:
-                              TextStyle(fontSize: 15, color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20,bottom: 5),
-                  child: Text('Forgot your password?',
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    fontFamily: 'SFUIDisplay',
-                    fontSize: 15,
-                    color: Colors.white,
-                  ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: MaterialButton(
-                    onPressed: () {/*Login Logic here*/},
-                    child: Text(
-                      'SIGN IN',
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontFamily: 'SFUIDisplay',
-                          fontWeight: FontWeight.bold),
-                    ),
-                    color: Color(0xffff2d55),
-                    elevation: 0,
-                    minWidth: 350,
-                    height: 60,
-                    textColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: MaterialButton(
-                    onPressed: () {},//Apply Login Logic Here
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Icon(FontAwesomeIcons.facebookSquare),
-                        Text(
-                          'Sign up with Facebook',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontFamily: 'SFUIDisplay',
-                          ),
-                        ),
                       ],
                     ),
-                    color: Colors.transparent,
-                    elevation: 0,
-                    minWidth: 350,
-                    height: 60,
-                    textColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                        side: BorderSide(color: Colors.white)),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: Center(
-                    child:RichText(
-    text: new TextSpan(
-    children: [
-    
-	new TextSpan(
-    text: "Don't have an account? ",
-    style: TextStyle(
-    fontFamily: 'SFUIDisplay',
-    color: Colors.white,
-    fontSize: 15,    
-    )
-    ),
-	new TextSpan(
-    text: "Sign up",
-    style: TextStyle(
-    fontFamily: 'SFUIDisplay',
-    color: Color(0xffff2d55),
-    fontSize: 15,
-    )
-    ),
-    ]
-  )
-) 
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20, bottom: 5),
+                    child: Text(
+                      Strings.forgot_psswd,
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontFamily: 'SFUIDisplay',
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
                     ),
-                )
-              ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: MaterialButton(
+                      onPressed: () {
+                        //TODO: Regular Login Logic
+                      },
+                      child: Text(
+                        Strings.login,
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontFamily: 'SFUIDisplay',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      color: Color(0xffff2d55),
+                      elevation: 0,
+                      minWidth: 350,
+                      height: 60,
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: MaterialButton(
+                      onPressed: () {
+                        //TODO: Facebook Login Logic
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Icon(FontAwesomeIcons.facebookSquare),
+                          Text(
+                            Strings.login_fb,
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontFamily: 'SFUIDisplay',
+                            ),
+                          ),
+                        ],
+                      ),
+                      color: Colors.transparent,
+                      elevation: 0,
+                      minWidth: 350,
+                      height: 60,
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                          side: BorderSide(color: Colors.white)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Center(
+                        child: RichText(
+                            text: new TextSpan(children: [
+                      new TextSpan(
+                          text: Strings.no_accont,
+                          style: TextStyle(
+                            fontFamily: 'SFUIDisplay',
+                            color: Colors.white,
+                            fontSize: 15,
+                          )),
+                      new TextSpan(
+                          text: Strings.sign_up,
+                          style: TextStyle(
+                            fontFamily: 'SFUIDisplay',
+                            color: Color(0xffff2d55),
+                            fontSize: 15,
+                          )),
+                    ]))),
+                  )
+                ],
+              ),
             ),
           ),
         ),
---
+++ lib/utils/utils.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Strings{
+  static const username = 'Username';
+  static const password = 'Password';
+  static const no_accont = "Don't have an account? ";
+  static const sign_up = "Sign up";
+  static const forgot_psswd = 'Forgot your password?';
+  static const login = 'LOGIN';
+  static const login_fb = 'Sign up with Facebook';
+}
