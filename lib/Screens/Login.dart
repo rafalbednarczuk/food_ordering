12d62b251709bb865874a2ec041d2c12262ed9ad
Maadhav <maadhav2001@gmail.com>
2019-11-02 10:05:47 +0530

    more changes made

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -13,147 +13,109 @@ class Login extends StatelessWidget {
         ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Container(
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.transparent,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(23.0),
-              child: ListView(
-                shrinkWrap: true,
-                children: <Widget>[
-                  SizedBox(
-                    height: 200,
-                  ),
-                  Form(
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 20),
-                          child: TextFormField(
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                            decoration: InputDecoration(
+          body: Padding(
+            padding: const EdgeInsets.all(23.0),
+            child: ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                SizedBox(
+                  height: 200,
+                ),
+                Form(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: TextFormField(
+                          style: Text_styles.textstyle,
+                          decoration: InputDecoration(
                               enabledBorder: const UnderlineInputBorder(
-                                borderSide: const BorderSide(
+                                borderSide: BorderSide(
                                   color: Colors.white,
                                 ),
                               ),
                               labelText: Strings.username,
-                              labelStyle:
-                                  TextStyle(fontSize: 15, color: Colors.white),
+                              labelStyle: Text_styles.textstyle),
+                        ),
+                      ),
+                      TextFormField(
+                        style: Text_styles.textstyle,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
                             ),
                           ),
+                          labelText: Strings.password,
+                          labelStyle: Text_styles.textstyle,
                         ),
-                        TextFormField(
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            enabledBorder: const UnderlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: Colors.white,
-                              ),
-                            ),
-                            labelText: Strings.password,
-                            labelStyle:
-                                TextStyle(fontSize: 15, color: Colors.white),
-                          ),
-                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 5),
+                  child: Text(Strings.forgot_psswd,
+                      textAlign: TextAlign.right, style: Text_styles.textstyle),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: MaterialButton(
+                    onPressed: () {
+                      //TODO: Regular Login Logic
+                    },
+                    child: Text(Strings.login, style: Text_styles.textstyle),
+                    color: Color(App_colors.login),
+                    elevation: 0,
+                    minWidth: 350,
+                    height: 60,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: MaterialButton(
+                    onPressed: () {
+                      //TODO: Facebook Login Logic
+                    },
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Icon(FontAwesomeIcons.facebookSquare),
+                        Text(Strings.login_fb, style: Text_styles.textstyle),
                       ],
                     ),
+                    color: Colors.transparent,
+                    elevation: 0,
+                    minWidth: 350,
+                    height: 60,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        side: BorderSide(color: Colors.white)),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 5),
-                    child: Text(
-                      Strings.forgot_psswd,
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        fontFamily: 'SFUIDisplay',
-                        fontSize: 15,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: MaterialButton(
-                      onPressed: () {
-                        //TODO: Regular Login Logic
-                      },
-                      child: Text(
-                        Strings.login,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Center(
+                      child: RichText(
+                          text: TextSpan(children: [
+                    TextSpan(
+                        text: Strings.no_accont, style: Text_styles.textstyle),
+                    TextSpan(
+                        text: Strings.sign_up,
                         style: TextStyle(
-                            fontSize: 15,
-                            fontFamily: 'SFUIDisplay',
-                            fontWeight: FontWeight.bold),
-                      ),
-                      color: Color(0xffff2d55),
-                      elevation: 0,
-                      minWidth: 350,
-                      height: 60,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: MaterialButton(
-                      onPressed: () {
-                        //TODO: Facebook Login Logic
-                      },
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          Icon(FontAwesomeIcons.facebookSquare),
-                          Text(
-                            Strings.login_fb,
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontFamily: 'SFUIDisplay',
-                            ),
-                          ),
-                        ],
-                      ),
-                      color: Colors.transparent,
-                      elevation: 0,
-                      minWidth: 350,
-                      height: 60,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          side: BorderSide(color: Colors.white)),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Center(
-                        child: RichText(
-                            text: new TextSpan(children: [
-                      new TextSpan(
-                          text: Strings.no_accont,
-                          style: TextStyle(
-                            fontFamily: 'SFUIDisplay',
-                            color: Colors.white,
-                            fontSize: 15,
-                          )),
-                      new TextSpan(
-                          text: Strings.sign_up,
-                          style: TextStyle(
-                            fontFamily: 'SFUIDisplay',
-                            color: Color(0xffff2d55),
-                            fontSize: 15,
-                          )),
-                    ]))),
-                  )
-                ],
-              ),
+                          fontFamily: 'SFUIDisplay',
+                          color: Color(App_colors.login),
+                          fontSize: 15,
+                        )),
+                  ]))),
+                )
+              ],
             ),
           ),
         ),
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Strings{
+class Strings {
   static const username = 'Username';
   static const password = 'Password';
   static const no_accont = "Don't have an account? ";
@@ -8,4 +8,16 @@ class Strings{
   static const forgot_psswd = 'Forgot your password?';
   static const login = 'LOGIN';
   static const login_fb = 'Sign up with Facebook';
+}
+
+class App_colors {
+  static const login = 0xffff2d55;
+}
+
+class Text_styles {
+  static const textstyle = TextStyle(
+    fontSize: 15,
+    color: Colors.white,
+    fontFamily: 'SFUIDisplay',
+  );
 }
